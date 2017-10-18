8bd5ddd7ce353aed03986fd4e14db9ff625e2c41
jackken123 <32786137+jackken123@users.noreply.github.com>
2017-10-18 21:12:28 +0800

    Add files via upload

---
+++ page/相關檔案/Chrono Engine(1).js
@@ -0,0 +1,22 @@
+	事件
+
+怪物  (路線:this.touchDamage(true)>向玩家移動)
+enemy_id : 1
+
+工具
+tool_item_id : 1
+tool_duration : 20
+tool_position : move_to_target
+tool_range : 1
+tool_area : line
+
+	資料庫
+
+道具
+Tool Id : 1
+
+怪物
+
+	按鍵
+Q:道具欄
+A:使用道具
